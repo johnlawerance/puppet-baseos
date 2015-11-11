7e6795e4793550a972c566da15f0893e389a93de
John Clark <john@johnlawerance.com>
2015-11-11 15:56:21 -0600

    Create users and groups

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,4 +7,9 @@ class baseos {
   $packages  = hiera_hash('packages')
   create_resources(package, $packages)
 
+  # Install users
+  $users = hiera_hash('users')
+  create_resources('@user', $users)
+  $groups = hiera_hash('groups')
+  create_resources('@group', $groups)
 }
