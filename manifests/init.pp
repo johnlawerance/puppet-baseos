08ea1013a1ae82137dbc3b2496eb235ae6d62cdd
John Clark <john@johnlawerance.com>
2015-11-18 11:33:11 -0600

    Managing ssh keys

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,9 +7,14 @@ class baseos {
   $packages  = hiera_hash('packages')
   create_resources(package, $packages)
 
-  # Install users
+  # Install users,groups / manage keys
   $users = hiera_hash('users')
   create_resources('user', $users)
+
   $groups = hiera_hash('groups')
   create_resources('group', $groups)
+
+  $ssh_authorized_keys = hiera_hash('ssh_authorized_keys')
+  create_resources('ssh_authorized_key', $ssh_authorized_keys)
+
 }
